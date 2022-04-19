7bb8fe32f0f04ede24c144159a049122affb004c
Minayimam <mina.yimam@go.minneapolis.edu>
2022-04-19 11:41:08 -0500

    Did chapter 11

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -12,24 +12,20 @@ import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
+
 class CrimeFragment : Fragment() {
 
-    // establishes the components before being initialized(buttons, edittext, etc.)
     private lateinit var  crime: Crime
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
 
-    // in the onCreate, crime will be established as a crime data class.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
     }
 
-    // in onCreateView, the titleField, dateButton, and solvedCheckBox are initialized.  The
-    // dateButton has a static string attached to it, and is set as false for the time being. The inflate
-    // is also stored in view, which will then be inflated after the components are initialized and
-    // view is returned.
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,7 +38,7 @@ class CrimeFragment : Fragment() {
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
-        // Sets the dateButton to a string, and is currently disabled.
+
         dateButton.apply{
             text = crime.date.toString()
             isEnabled = false
